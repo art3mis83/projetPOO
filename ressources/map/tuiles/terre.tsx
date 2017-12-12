778f300ae44a5821047e8ad93a396413111708cf
art3mis83 <33057406+art3mis83@users.noreply.github.com>
2017-12-12 15:23:27 +0100

    Add files via upload

---
+++ ressources/map/tuiles/terre.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="terre" tilewidth="32" tileheight="32" tilecount="108" columns="12">
+ <image source="tuiles/terre.png" width="384" height="288"/>
+</tileset>
